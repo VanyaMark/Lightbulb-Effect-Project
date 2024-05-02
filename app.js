13c339a9266321625e2bcf7b2045a79e72e90ada
VanyaMark <markova.vanya@gmail.com>
2024-05-02 21:01:41 +0200

    done

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // Buena suerte ! Espero que se te encianda la "bombilla"!
+
+const bulb = document.querySelector('#bulb')
+const toggleBulb = () => bulb.className === 'bulb-off' ? bulb.className = 'bulb-on': bulb.className = 'bulb-off';
+
+bulb.onclick = toggleBulb;
